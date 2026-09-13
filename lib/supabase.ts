6744f33ad95b658d9965a6518aa9a7637fab13a5
brianwalker1109-source <brianwalker1109@gmail.com>
2026-09-13 22:07:20 +0100

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,3 @@
+import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+export { createClientComponentClient, createServerComponentClient, cookies }
